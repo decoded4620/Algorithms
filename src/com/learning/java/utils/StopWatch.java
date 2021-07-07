0d34838ee4929ca711ab61d09920e06209e8d6f1
Bow Archer <barcher4620@gmail.com>
2021-07-07 11:21:28 -0700

    Add pause capability to StopWatch

--- src/com/learning/java/utils/StopWatch.java
+++ src/com/learning/java/utils/StopWatch.java
@@ -1,18 +1,92 @@
 package com.learning.java.utils;
 
+import java.util.concurrent.TimeUnit;
+
+
 /**
  * Very simple single use stopwatch for timing code blocks.
  */
 public class StopWatch {
-    private long _startTime = 0;
+    private boolean _paused = false;
+    private boolean _running = false;
 
-    public void start() {
-        _startTime = System.nanoTime();
+    private long _startTime = 0;
+    private long _lastPausedTime = 0;
+    private long _pausedTotalTime = 0;
+
+    /**
+     * Returns current running time, less time that we've been paused, if any.
+     *
+     * @param timeUnit the {@link TimeUnit}
+     * @return a double value, in the specified {@link TimeUnit}
+     */
+    public double currentTime(TimeUnit timeUnit) {
+        var totalNanos = System.nanoTime() - _startTime - _pausedTotalTime;
+        switch (timeUnit) {
+            case NANOSECONDS:
+                return totalNanos;
+            case MICROSECONDS:
+                return totalNanos / 1_000.0;
+            case MILLISECONDS:
+                return totalNanos / 1_000_000.0;
+            case SECONDS:
+                return totalNanos / 1_000_000_000.0;
+            case MINUTES:
+                return totalNanos / 1_000_000_000.0 / 60.0;
+            case HOURS:
+                return totalNanos / 1_000_000_000.0 / 60.0 / 60.0;
+            default:
+                throw new UnsupportedOperationException("Time unit " + timeUnit + " is not supported");
+        }
     }
 
+    /**
+     * Start only if not already running
+     */
+    public void start() {
+        if (!_running)
+        {
+            _running = true;
+            _startTime = System.nanoTime();
+        }
+    }
+
+    /**
+     * Pause or unpause, only if already running
+     */
+    public long pause() {
+        if (_running) {
+            if(!_paused) {
+                _lastPausedTime = System.nanoTime();
+            } else {
+                _pausedTotalTime += System.nanoTime() - _lastPausedTime;
+                _lastPausedTime = 0;
+            }
+            _paused = !_paused;
+        }
+
+        return _pausedTotalTime;
+    }
+
+    /**
+     * Stop, only if already running. Paused state does not matter.
+     * @return the total running time, less time paused.
+     */
     public long stop() {
-        var val = System.nanoTime() - _startTime;
-        _startTime = 0;
-        return val;
+        if (_running) {
+            if (_paused) {
+                // unpause and record total paused time.
+                this.pause();
+            }
+
+            var totalTime = System.nanoTime() - _startTime - _pausedTotalTime;
+            // reset states, total time is reset upon next start
+            _startTime = 0;
+            _pausedTotalTime = 0;
+            _running = false;
+            return totalTime;
+        }
+
+        return 0;
     }
 }
